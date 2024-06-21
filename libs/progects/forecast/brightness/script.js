bb80a7c31b09b7ffd51d23976bef4c78bf2669f2
Milsdf <milanalatova@gmail.com>
2024-06-21 14:45:09 +0300

    Delete libs directory

--- libs/progects/forecast/brightness/script.js
+++
@@ -1,6 +0,0 @@
-rangeInput = document.getElementById("range");
-continer = document.getElementsByClassName("conntainer")[0];
-
-rangeInput.addEventListener("change", function (){
-    continer.style.filter = "brightness("+rangeInput.value + "%)";
-})
